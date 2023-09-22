1cd8384139e1bb1234ff37f1f688d328122eab38
Pawan Yadav <55851664+starydv7@users.noreply.github.com>
2023-09-22 20:28:40 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,8 @@ import { ClerkProvider } from '@clerk/nextjs'
 
 
 export const metadata: Metadata = {
-<<<<<<< Updated upstream
-  title: 'Saket E-commerce Website',
-  description: 'Saket Admin Dashbaord',
-=======
-  title: 'Saket Admin Dashbaord',
+  title: 'Saket E-commerce',
   description: 'Saket E-COMMERCE',
->>>>>>> Stashed changes
 }
 
 export default function RootLayout({
