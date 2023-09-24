23a54e8bb8f058535cc32b7d5a646d4fadb21594
Pawan Yadav <55851664+starydv7@users.noreply.github.com>
2023-09-24 22:51:23 +0530

    page tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 const SetupPage=()=> {
   return (
    <>
-   <div className='p-3 w-full'>
+   <div className='p-10w-full'>
   <UserButton afterSignOutUrl="/"/>
    </div>
    </>
